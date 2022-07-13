fc4ea058794a041e90109837a75761746b0fa6f7
macanotmarker <macasorekingdavid@gmail.com>
2022-07-13 01:47:58 +0100

    update main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdarg.h>
+/*Structs*/
+/*Typedefs*/
+/*Prototypes*/
+int _putchar(char c, int *s);
+int _printf(const char *format, ...);
+#endif
