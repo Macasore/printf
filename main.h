bdd4a1b72e6902e8da2bbdcdab9155bd9af8bea6
macanotmarker <macasorekingdavid@gmail.com>
2022-07-13 02:10:02 +0100

    update main.h

--- main.h
+++ main.h
@@ -5,5 +5,11 @@
 /*Typedefs*/
 /*Prototypes*/
 int _putchar(char c, int *s);
+void _puts(char *str, int *k);
 int _printf(const char *format, ...);
+void _print_number(int n, int *k);
+void _rev_string(char *s, int *k);
+int _strlen(char *s);
+void _print_binary(int n, int *k);
+void _rot13(char *s, int *k);
 #endif
