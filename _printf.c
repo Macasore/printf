03d3400224e0e34354d744aa781294b0de5ea85c
Babsmutini <scotteyawo@gmail.com>
2022-07-13 03:46:13 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -1,128 +1,44 @@
 #include "main.h"
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
- *
- * Return: 1 if digit, 0 otherwise
+ * _printf - prints anything
+ * @format: format
+ * Return: number of characters printed
  */
-int _isdigit(int c)
+int _printf(const char *format, ...)
 {
-return (c >= '0' && c <= '9');
+int sum = 0;
+va_list list;
+char *s, *cr;
+p_t par = PARAMS_INIT;
+va_start(list, format);
+if (!format || (format[0] == '%' && !format[1]))
+return (-1);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
+return (-1);
+for (s = (char *)format; *s; s++)
+{
+init_params(&par, list);
+if (*s != '%')
+{
+sum += _putchar(*s);
+continue;
 }
-
-/**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
- */
-int _strlen(char *s)
-{
-int i = 0;
-
-while (*s++)
-i++;
-return (i);
+cr = s;
+s++;
+while (get_flag(s, &par))
+s++;
 }
-/**
- * print_number - prints a number with options
- * @str: the base number as a string
- * @par: the parameter struct
- *
- * Return: chars printed
- */
-int print_number(char *str, p_t *par)
-{
-unsigned int i;
-int neg;
-i = _strlen(str);
-neg = (!par->unsign && *str == '-');
-if (!par->precision && *str == '0' && !str[1])
-str = "";
-if (neg)
-{
-str++;
-i--;
-}
-if (par->precision != UINT_MAX)
-while (i++ < par->precision)
-*--str = '0';
-if (neg)
-*--str = '-';
-
-if (!par->minus_flag)
-return (print_number_right_shift(str, par));
+s = get_width(s, &par, list);
+s = get_precision(s, &par, list);
+if (get_modifier(s, &par))
+s++;
+if (!get_specifier(s))
+sum += print_from_to(cr, s,
+par.l_modifier || par.h_modifier ? s - 1 : 0);
 else
-return (print_number_left_shift(str, par));
+sum += get_print_func(s, list, &par);
 }
-
-/**
- * print_number_right_shift - prints a number with options
- * @str: the base number as a string
- * @par: the parameter struct
- * Return: chars printed
- */
-int print_number_right_shift(char *str, p_t *par)
-{
-unsigned int n = 0, neg, neg2, i = _strlen(str);
-char pad_char = ' ';
-if (par->zero_flag && !par->minus_flag)
-pad_char = '0';
-neg = neg2 = (!par->unsign && *str == '-');
-if (neg && i < par->width && pad_char == '0' && !par->minus_flag)
-str++;
-else
-neg = 0;
-if ((par->plus_flag && !neg2) ||
-(!par->plus_flag && par->space_flag && !neg2))
-i++;
-if (neg && pad_char == '0')
-n += _putchar('-');
-if (par->plus_flag && !neg2 && pad_char == '0' && !par->unsign)
-n += _putchar('+');
-else if (!par->plus_flag && par->space_flag && !neg2 &&
-!par->unsign && par->zero_flag)
-n += _putchar(' ');
-while (i++ < par->width)
-n += _putchar(pad_char);
-if (neg && pad_char == ' ')
-n += _putchar('-');
-if (par->plus_flag && !neg2 && pad_char == ' ' && !par->unsign)
-n += _putchar('+');
-else if (!par->plus_flag && par->space_flag && !neg2 &&
-!par->unsign && !par->zero_flag)
-n += _putchar(' ');
-n += _puts(str);
-return (n);
+_putchar(BUF_FLUSH);
+va_end(list);
+return (sum);
 }
-
-/**
- * print_number_left_shift - prints a number with options
- * @str: the base number as a string
- * @par: the parameter struct
- *
- * Return: chars printed
- */
-int print_number_left_shift(char *str, p_t *par)
-{
-unsigned int n = 0, neg, neg2, i = _strlen(str);
-char pad_char = ' ';
-
-if (par->zero_flag && !par->minus_flag)
-pad_char = '0';
-neg = neg2 = (!par->unsign && *str == '-');
-if (neg && i < par->width && pad_char == '0' && !par->minus_flag)
-str++;
-else
-neg = 0;
-
-if (par->plus_flag && !neg2 && !par->unsign)
-n += _putchar('+'), i++;
-else if (par->space_flag && !neg2 && !par->unsign)
-n += _putchar(' '), i++;
-n += _puts(str);
-while (i++ < par->width)
-n += _putchar(pad_char);
-return (n);
-}
-
