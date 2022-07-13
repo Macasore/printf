2b8af0a222ccda1b33546183f46baf479095697c
Babsmutini <scotteyawo@gmail.com>
2022-07-13 01:34:31 +0100

    first putchar

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include"main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to be printed
+ * @s: counter through string
+ * Return: One or -1 if it is error
+ */
+int _putchar(char c, int *s)
+{
+	*s += 1;
+	return (write(1, &c, 1));
+}
